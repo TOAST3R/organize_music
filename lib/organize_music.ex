a6f0650d632b0e6da922f6a8057a8bed20c283bc
TOAST3R <pabloarean@gmail.com>
2016-11-19 01:15:12 +0100

    make private methods use :logger and fix rename_directory

--- lib/organize_music.ex
+++ lib/organize_music.ex
@@ -1,5 +1,3 @@
-require Logger
-
 defmodule OrganizeMusic do
   def refactor_directories(music_folder_path) do
     case File.cd(music_folder_path) do
@@ -8,40 +6,46 @@ defmodule OrganizeMusic do
         directories
         |> Enum.filter(&(File.dir?(&1)))
         |> Enum.map(&(rename_directory(&1)))
+     
       {:error, :enoent} ->
-        Logger.error "#{music_folder_path} it is not a valid absolute path"
+        :logger.error "#{music_folder_path} it is not a valid absolute path"
     end
   end
 
-  def band_name(directory_name) do
+  def rename_directory(directory_name) do
+    case band_name(directory_name) do
+      {:ok, ^directory_name} -> 
+        :logger.debug "'#{directory_name}' folder is correct already"
+      {:ok, band_name} -> 
+        File.mkdir(band_name)
+        File.cd(band_name)
+
+        case album_name(directory_name) do
+          {:ok, album_name} ->
+          	new_album_path = band_name <> "/" <> album_name
+            File.rename(directory_name, new_album_path)
+            :logger.debug "new directory name: #{new_album_path}"
+            File.cd("..")
+
+          {:error, _} ->
+            :logger.error "directory_name: #{directory_name}"
+            File.cd("..")
+        end
+    end
+  end
+
+  defp band_name(directory_name) do
     directory_name
     |> String.split("-")
     |> Enum.fetch(0)
   end
   
-  def album_name(directory_name) do
+  defp album_name(directory_name) do
     directory_name
     |> String.split("-")
     |> Enum.drop(1)
     |> Enum.concat
     |> String.downcase
     |> String.replace(~r/\[|\]|\(|\)/, "")
-  end
-
-  def rename_directory(directory_name) do
-    case band_name(directory_name) do
-      {:ok, directory_name} -> 
-        Logger.debug "'#{directory_name}' folder is correct already"
-      {:ok, band_name} -> 
-        File.mkdir(band_name)
-        
-        case album_name(directory_name) do
-          {:ok, album_name} ->
-            Logger.debug "new directory name: #{band_name <> "/" <> album_name}"
-            File.rename(directory_name, band_name <> "/" <> album_name)
-          {:error, _} ->
-            Logger.error "directory_name: #{directory_name}"
-        end
-    end
   end
 end
