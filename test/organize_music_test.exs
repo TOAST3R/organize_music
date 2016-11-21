e2044026d3a04c7db101ade0b3b7c0903d08174c
TOAST3R <pabloarean@gmail.com>
2016-11-21 21:51:42 +0100

    refactor tests

--- test/organize_music_test.exs
+++ test/organize_music_test.exs
@@ -2,32 +2,29 @@ defmodule OrganizeMusicTest do
   use ExUnit.Case
   doctest OrganizeMusic
 
-  test "release with all meta data" do
-    directory = "Zeta Reticula - EP 5 (2016)"
-    assert OrganizeMusic.band_name(directory), "zeta reticula"
-    assert OrganizeMusic.release_without_year(directory), "ep 5"
-    assert OrganizeMusic.year(directory), "2016"
+  test "band_name" do
+    assert OrganizeMusic.band_name("Zeta Reticula - EP 5 (2016)"), "zeta reticula"
+    assert OrganizeMusic.band_name("Zeta Reticula - EP 5"), "zeta reticula"
+    assert OrganizeMusic.band_name("Zeta Reticula - [2016]"), "zeta reticula"
+    assert OrganizeMusic.band_name("Zeta Reticula"), "zeta reticula"
+  end
+  
+  test "release_without_year" do
+    assert OrganizeMusic.release_without_year("Zeta Reticula - EP 5 (2016)"), "ep 5"
+    assert OrganizeMusic.release_without_year("Zeta Reticula - EP 5"), "ep 5"
+    assert OrganizeMusic.release_without_year("Zeta Reticula - [2016]"), ""
+    assert OrganizeMusic.release_without_year("Zeta Reticula"), ""
+  end
+
+  test "year" do
+  	assert OrganizeMusic.year("Zeta Reticula - EP 5 (2016)"), "2016"
+    assert OrganizeMusic.year("Zeta Reticula - EP 5"), ""
+    assert OrganizeMusic.year("Zeta Reticula - [2016]"), "2016"
+    assert OrganizeMusic.year("Zeta Reticula"), ""
+  end
+
+  test "compose_folder_name" do
     assert OrganizeMusic.compose_folder_name("zeta reticula", "2016", "ep 5"), "zeta reticula/(2016) ep 5"
-  end
-
-  test "release without year" do
-    directory = "Zeta Reticula - EP 5"
-    assert OrganizeMusic.band_name(directory), "zeta reticula"
-    assert OrganizeMusic.release_without_year(directory), "ep 5"
-    assert OrganizeMusic.year(directory), ""
-  end
-
-  test "release without album" do
-    directory = "Zeta Reticula - [2016]"
-    assert OrganizeMusic.band_name(directory), "zeta reticula"
-    assert OrganizeMusic.release_without_year(directory), ""
-    assert OrganizeMusic.year(directory), "2016"
-  end
-
-  test "release without album and year" do
-    directory = "Zeta Reticula"
-    assert OrganizeMusic.band_name(directory), "zeta reticula"
-    assert OrganizeMusic.release_without_year(directory), ""
-    assert OrganizeMusic.year(directory), ""
+    assert OrganizeMusic.compose_folder_name("zeta reticula", "", "ep 5"), "zeta reticula/ep 5"
   end
 end
