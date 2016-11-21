1cca1cfd07aa21fe8fdb7a359b8de4c811aa8eaa
TOAST3R <pabloarean@gmail.com>
2016-11-21 21:53:17 +0100

    more refactor

--- test/organize_music_test.exs
+++ test/organize_music_test.exs
@@ -3,12 +3,12 @@ defmodule OrganizeMusicTest do
   doctest OrganizeMusic
 
   test "band_name" do
-    assert OrganizeMusic.band_name("Zeta Reticula - EP 5 (2016)"), "zeta reticula"
+    assert OrganizeMusic.band_name("ZETA RETICULA - EP 5 (2016)"), "zeta reticula"
     assert OrganizeMusic.band_name("Zeta Reticula - EP 5"), "zeta reticula"
     assert OrganizeMusic.band_name("Zeta Reticula - [2016]"), "zeta reticula"
     assert OrganizeMusic.band_name("Zeta Reticula"), "zeta reticula"
   end
-  
+
   test "release_without_year" do
     assert OrganizeMusic.release_without_year("Zeta Reticula - EP 5 (2016)"), "ep 5"
     assert OrganizeMusic.release_without_year("Zeta Reticula - EP 5"), "ep 5"
